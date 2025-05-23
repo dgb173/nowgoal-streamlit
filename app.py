149f614ad46b9e12aa92033418a5ba267a6cab9c
dgb173 <dgb173.fotos@gmail.com>
2025-05-23 22:31:13 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 
 import streamlit as st
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+
 from selenium.webdriver.chrome.options import Options
 from bs4 import BeautifulSoup
 import re
